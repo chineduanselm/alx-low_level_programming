334ad7aa45bc0180204d2b7d7d92a2c54cfccd00
chineduanselm <chinedumafoama@yahoo.com>
2023-06-19 10:05:17 -0700

    programs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 to end function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
